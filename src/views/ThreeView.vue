d3032260fbefe92fdfcd5562a822a362fcdd0399
lucas-faget <lucas.faget@laposte.net>
2024-04-23 13:38:47 +0200

    Create 3D chessboard and render it in the scene, enable camera controls

--- src/views/ThreeView.vue
+++ src/views/ThreeView.vue
@@ -1,34 +1,76 @@
 <script>
 	import * as THREE from 'three';
-  
+	import { MapControls } from 'three/examples/jsm/controls/MapControls.js';
+	
 	export default {
 		mounted() {
 			const scene = new THREE.Scene();
 			const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+			const renderer = new THREE.WebGLRenderer({ alpha: true });
+			const controls = new MapControls(camera, renderer.domElement);
 
-			let renderer = new THREE.WebGLRenderer();
 			renderer.setSize(window.innerWidth, window.innerHeight);
-
+			renderer.setClearColor(0x000000, 0);
 			this.$refs.container.appendChild(renderer.domElement);
 
-			const geometry = new THREE.BoxGeometry(1, 1, 1);
-			const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-
-			const cube = new THREE.Mesh(geometry, material);
-			scene.add(cube);
-
-			camera.position.z = 5;
-
-			const animate = () => {
-				requestAnimationFrame(animate);
-
-				cube.rotation.x += 0.01;
-				cube.rotation.y += 0.01;
-
-				renderer.render(scene, camera);
+			controls.mouseButtons = {
+				LEFT: THREE.MOUSE.LEFT,
+				RIGHT: THREE.MOUSE.LEFT
 			};
+			controls.enablePan = false;
+			controls.enableDamping = true;
+			controls.dampingFactor = 0.05;
+			controls.screenSpacePanning = false;
+			controls.minDistance = 50;
+			controls.maxDistance = 150;
+			controls.maxPolarAngle = Math.PI / 2.5;
+
+			this.addAxis(scene);
+			this.addChessboard(scene, camera, renderer, controls);
+
+			camera.position.set(50, 50, 50);
+			camera.lookAt(0, 0, 0);
+
+			function animate() {
+				requestAnimationFrame(animate);
+				controls.update(); // only required if controls.enableDamping = true, or if controls.autoRotate = true
+				renderer.render(scene, camera);
+			}
+
+			function onWindowResize() {
+				camera.aspect = window.innerWidth / window.innerHeight;
+				camera.updateProjectionMatrix();
+				renderer.setSize(window.innerWidth, window.innerHeight);
+			}
+
+			window.addEventListener('resize', onWindowResize);
 
 			animate();
+		},
+		methods: {
+			addAxis(scene) {
+				const axesHelper = new THREE.AxesHelper(100);
+				scene.add(axesHelper);
+			},
+			addChessboard(scene) {
+				const squareNumber = 8;
+				const squareSize = 10;
+
+				const geometry = new THREE.BoxGeometry(squareSize, 2, squareSize);
+				const lightMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
+				const darkMaterial = new THREE.MeshBasicMaterial({ color: 0x666666 });
+
+				for (let x = 0; x < 8; x++) {
+					for (let y = 0; y < 8; y++) {
+						let material = (x + y) % 2 == 0 ? lightMaterial : darkMaterial;
+						let square = new THREE.Mesh(geometry, material);
+						let xx = squareSize * (x - squareNumber / 2 + 0.5);
+						let yy = squareSize * (y - squareNumber / 2 + 0.5);
+						square.position.set(xx, 0, yy);
+						scene.add(square);
+					}
+				}
+			}
 		}
 	}
 </script>
@@ -36,8 +78,7 @@
 <template>
 	<div ref="container"></div>
 </template>
-  
+
 <style scoped>
 
 </style>
-  
